269faa4271091eb8259a7d94a12f45b7d6bde3b3
Manochatt <artmanochjo2501@gmail.com>
2024-06-12 09:53:25 +0700

    [TS] hamming weight

---
+++ num1.ts
@@ -0,0 +1,12 @@
+function hammingWeight(n: number): number {
+    let ans = 0
+    while(n !== 0) {
+        ans += n%2
+        n = Math.floor(n/2)
+        console.log(n)
+    }
+
+    return ans
+};
+
+console.log(hammingWeight(11))
